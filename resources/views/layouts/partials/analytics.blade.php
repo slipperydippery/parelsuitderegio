4b3f3cb4d3af7ee1b2e4be84bcea68fbae23ea58
Maarten de Jager <maartendejager@gmail.com>
2018-11-29 12:10:22 +0000

    updates analytics to anonymize IP

--- resources/views/layouts/partials/analytics.blade.php
+++ resources/views/layouts/partials/analytics.blade.php
@@ -1,12 +1,9 @@
-<script>
-
 	<!-- Global site tag (gtag.js) - Google Analytics -->
 	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-93563705-1"></script>
 	<script>
-	  window.dataLayer = window.dataLayer || [];
-	  function gtag(){dataLayer.push(arguments);}
-	  gtag('js', new Date());
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
 
-	  gtag('config', 'UA-93563705-1', { ‘anonymize_ip’: true });
-
-</script>
+		gtag('config', 'UA-93563705-1', { ‘anonymize_ip’: true });
+	</script>
